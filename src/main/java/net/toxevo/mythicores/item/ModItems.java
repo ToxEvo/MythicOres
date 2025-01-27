e5c2759d6487432ee40c88a28c2fd2f03334dd37
ToxEvo <dawsalowy@gmail.com>
2025-01-27 01:20:13 +0000

    Added: test item

--- src/main/java/net/toxevo/mythicores/item/ModItems.java
+++ src/main/java/net/toxevo/mythicores/item/ModItems.java
@@ -1,12 +1,18 @@
 package net.toxevo.mythicores.item;
 
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.toxevo.mythicores.MythicOres;
 
 public class ModItems
 {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MythicOres.MOD_ID);
+
+    //Example Item
+    public static final DeferredItem<Item> TEST_ITEM = ITEMS.register("test_item",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus)
     {
